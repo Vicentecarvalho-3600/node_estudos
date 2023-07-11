38dfa8f226630578886bc2ca5f9aac996cf40638
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-07-11 20:41:33 -0300

    refatorando codigo

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-const express = require('express');
-const msg = require('./mod_teste');
-const app = express();
+const app = require('./config/server');
 
-app.set('view engine', 'ejs');
+const rotasNoticias = require('./app/routes/home')(app);
 
-app.get('/', (req, res)=>{
-    res.send('home/index');
-});
+const rotahome = require('./app/routes/noticias')(app);
 
-app.get('/formulario_inclucao_noticia', (req, res)=>{
-    res.render('admin/form_add_noticia');
-});
-
-app.get('/noticias', (req, res)=>{
-    res.send('noticias/noticias');
-});
-
-app.get('/beleza', (req, res)=>{
-    res.send("<html><body><h1>Beleza</h1><body></html>");
-});
+const rotaformInclucao = require('./app/routes/formulario_inclucao_noticia')(app);
 
 app.listen(3333,()=>{
     console.log('servidor online');
     console.log('http://localhost:3333');
-    console.log(msg());
 });
