5e1872c76723c828d0ad4cd031cdd67a47b8bac3
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-07-11 21:16:35 -0300

    trabanhando com mysql no express

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,19 @@
 module.exports = (app) => {
-    app.get('/noticias', (req, res)=>{
-        res.render('noticias/noticias');
+
+    app.get('/noticias', (req, res) => {
+        const mysql = require('mysql');
+
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', (erro, result) => {
+            res.render('noticias/noticias', {noticias: result});
+        });
+
+        
     });
 };
