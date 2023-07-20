614f62f6af945ec490dfadc02a6af08d76ce1226
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-07-20 02:34:19 -0300

    refactoring do metodo de conexao com banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,21 +1,14 @@
+const dbConnection  = require('../../config/dbConnection');
+
 module.exports = (app) => {
 
+    const connection = dbConnection();
+
     app.get('/noticias', (req, res) => {
-
-        const mysql = require('mysql');
-
-        const connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'portal_noticias'
-        });
 
         connection.query('select * from noticias', (erro, result) => {
             res.render('noticias/noticias', {noticias: result});
         });
-        
-        
-        
+    
     });
 };
---
+++ config/dbConnection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+
+module.exports = ()=> {
+    return connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'portal_noticias'
+    });
+};
